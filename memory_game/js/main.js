9576898e1503b4ddf73b9c1273618b7e5f23a1bb
ricenzca <ricenzca@hotmail.com>
2019-03-05 17:27:34 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,15 +30,26 @@ var checkForMatch = function() {
 		}
 }
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+var flipCard = function() {
+	var cardID = this.getAttribute('data-id')
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
+	this.setAttribute('src', cards[cardID].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
